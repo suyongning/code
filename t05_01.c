e873da70e8e6b6f7cc4cf3a9bc5d226bee5e9944
suyongning <a26i072jm@ous.jp>
2026-06-24 08:04:23 +0800

    t05_01

--- t05_01.c
+++ t05_01.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main(void) 
@@ -11,18 +10,4 @@ int main(void)
    else
        puts ("入力はゼロではありません。");
     return 0;
-=======
-#include <stdio.h>
-
-int main(void) 
-{
-   int num;
-   printf("入力してください：") ;
-   scanf("%d", &num); 
-   if (num == 0)
-       puts ("入力はゼロです。");
-   else
-       puts ("入力はゼロではありません。");
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
