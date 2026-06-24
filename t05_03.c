475e0a84ba54c7d26ab5f3895f3bc8151bd83898
suyongning <a26i072jm@ous.jp>
2026-06-24 08:04:45 +0800

    t05_03

--- t05_03.c
+++ t05_03.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main(void) 
@@ -8,15 +7,4 @@ int main(void)
     scanf("%d", &a);
     printf(a >= 20 ? "お酒が飲めます。\n" : "お酒は二十歳になってから。\n");
     return 0;
-=======
-#include <stdio.h>
-
-int main(void) 
-{
-    int a;
-    printf("年齢を入力してください: ");
-    scanf("%d", &a);
-    printf(a >= 20 ? "お酒が飲めます。\n" : "お酒は二十歳になってから。\n");
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
