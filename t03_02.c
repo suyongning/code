976b5fe788b506eafbede503ff8df80fc7fbc8c0
suyongning <a26i072jm@ous.jp>
2026-06-24 08:02:53 +0800

    t03_02

--- t03_02.c
+++ t03_02.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 
 int main(void)
@@ -9,16 +8,4 @@ int main(void)
 
     printf("整数: %d\n",num);
     return 0;
-=======
-#include<stdio.h>
-
-int main(void)
-{
-    int num;
-    printf("整数を入力してください:  ");
-    scanf("%d",&num);
-
-    printf("整数: %d\n",num);
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
