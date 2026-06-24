4bfdfa10fed50b5002b5f4da1a9e4d3bd2628011
suyongning <a26i072jm@ous.jp>
2026-06-24 08:05:23 +0800

    t06_03

--- t06_03.c
+++ t06_03.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main(void)
@@ -15,22 +14,4 @@ int main(void)
             printf("10より小さいまたは40より大きい。");
         }
     return 0;
-=======
-#include <stdio.h>
-
-int main(void)
-{
-    int a;
-    printf("整数値を入力してください：");
-    scanf("%d", &a);
-        if (a > 20 && a < 30)
-        {
-            printf("20以上かつ30以下です。");
-        }
-        if (a < 10 || a > 40)
-        {
-            printf("10より小さいまたは40より大きい。");
-        }
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
