e9d35e49a87cd24b92e41d385fcef8efbdadbdc9
suyongning <a26i072jm@ous.jp>
2026-06-24 08:04:32 +0800

    t05_02

--- t05_02.c
+++ t05_02.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main(void) 
@@ -13,20 +12,4 @@ int main(void)
        else
        puts ("a == bは 0 です。\na != bは 1 です。");
     return 0;
-=======
-#include <stdio.h>
-
-int main(void) 
-{
-    int a , b;
-    printf("整数a:");
-    scanf("%d", &a);
-    printf("整数b:");
-    scanf("%d", &b);
-    if (a == b)
-       puts ("a == bは 1 です。\na != bは 0 です。");
-       else
-       puts ("a == bは 0 です。\na != bは 1 です。");
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
