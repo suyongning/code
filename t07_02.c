ddf5e5f6975f84ea1f102b5aef49fdfb3bfdb0be
suyongning <a26i072jm@ous.jp>
2026-06-24 08:05:43 +0800

    t07_02

--- t07_02.c
+++ t07_02.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 
 int main(void)
@@ -15,22 +14,4 @@ int main(void)
         default: printf("%d\n", a); break;
     }
     return 0;
-=======
-#include<stdio.h>
-
-int main(void)
-{
-    int a;
-    printf("整数値を入力してください：");
-    scanf("%d", &a);
-    
-    switch(a)
-    {
-        case 11: printf("JACK\n"); break;
-        case 12: printf("QUEEN\n"); break;
-        case 13: printf("KING\n"); break;
-        default: printf("%d\n", a); break;
-    }
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
