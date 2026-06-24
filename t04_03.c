0a9c3cf3d31e0b3f60b0358f32e332f942279d17
suyongning <a26i072jm@ous.jp>
2026-06-24 08:04:11 +0800

    t04_03

--- t04_03.c
+++ t04_03.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 
 int main(void)
@@ -10,17 +9,4 @@ int main(void)
     scanf("%lf", &b);
     printf("BMIは %lf です。", b / (a / 100)/(a / 100));
      return 0;
-=======
-#include<stdio.h>
-
-int main(void)
-{
-   double a , b;
-   printf("身長と体重を入力してください。\n身長:");
-    scanf("%lf", &a);
-    printf("体重kg:");
-    scanf("%lf", &b);
-    printf("BMIは %lf です。", b / (a / 100)/(a / 100));
-     return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
