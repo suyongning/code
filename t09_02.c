efac26d47d142afc5d3e48eddafd9a0fc3b50410
suyongning <a26i072jm@ous.jp>
2026-06-24 08:06:47 +0800

    t09_02

--- t09_02.c
+++ t09_02.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 int main(void)
 {
@@ -11,18 +10,4 @@ int main(void)
     }
     printf("%d", sum);
     return 0;
-=======
-#include<stdio.h>
-int main(void)
-{
-    int a, sum = 0;
-    printf("整数値を入力してください：");
-    scanf("%d", &a);
-    for(int b=1; b <= a; b++)
-    {
-        sum = sum + b;
-    }
-    printf("%d", sum);
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
