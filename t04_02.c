6b25a4b2cd37dd0638b0539d7f5db638cd61be14
suyongning <a26i072jm@ous.jp>
2026-06-24 08:03:57 +0800

    t04_02

--- t04_02.c
+++ t04_02.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 
 int main(void)
@@ -12,19 +11,4 @@ int main(void)
     printf("2の数は %d です。\n", c);
 
     return 0;
-=======
-#include<stdio.h>
-
-int main(void)
-{
-    int a;
-    printf("整数を入力してください。\n");
-    printf("整数 : ");
-    scanf("%d", &a);
-    int b = a / 10;
-    int c = b % 10;
-    printf("2の数は %d です。\n", c);
-
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
