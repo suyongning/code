adef745dc803bd12aa6e757b0761cc1f7bbf2d85
suyongning <a26i072jm@ous.jp>
2026-06-24 08:05:33 +0800

    t07_01

--- t07_01.c
+++ t07_01.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main(void)
@@ -15,22 +14,4 @@ int main(void)
         break;
     }
     return 0;
-=======
-#include <stdio.h>
-
-int main(void)
-{
-    int a;
-    printf("整数値を入力してください：");
-    scanf("%d", &a);
-    switch (a % 2)
-    {    case 0:
-        printf("偶数です。");
-        break;
-    case 1:
-        printf("奇数です。");
-        break;
-    }
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
