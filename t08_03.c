399d848b3c12929b3583bd8d62a26fac616d1c5b
suyongning <a26i072jm@ous.jp>
2026-06-24 08:06:27 +0800

    t08_03

--- t08_03.c
+++ t08_03.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 int main(void)
 {
@@ -17,24 +16,4 @@ int main(void)
              a--;
             }
     return 0;
-=======
-#include<stdio.h>
-int main(void)
-{
-        int a;
-        printf("整数値を入力してください：");
-        scanf("%d",&a);
-            while(a>=0)
-            {
-            if(a % 2 != 0)
-            {
-                a--;
-                continue;
-            }
-            
-             printf("%d\n",a);
-             a--;
-            }
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
