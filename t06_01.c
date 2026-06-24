312353a76fe1b5b540e0886d904def765bafbfce
suyongning <a26i072jm@ous.jp>
2026-06-24 08:05:00 +0800

    t06_01

--- t06_01.c
+++ t06_01.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main(void)
@@ -16,23 +15,4 @@ int main(void)
     else{
     }
     return 0;
-=======
-#include <stdio.h>
-
-int main(void)
-{
-    int a;
-    printf("小遣いの残高を入力してください：");
-    scanf("%d", &a);
-    if (a >= 1000)
-    {
-        int b;
-        printf("いくら使いますか：");
-        scanf("%d", &b);
-        printf("残りは%d円です", a - b);
-    }
-    else{
-    }
-    return 0;
->>>>>>> ca3446fd9ee3e893fdaf32236b6840f172d2db18
 }
